c244a9b046a9639af591b0e4d52852eb6b4c0cff
Hyzyr <avezberdiyew@gmail.com>
2023-01-12 20:34:28 +0500

    articles-xsm

---
+++ assets/js/articles.js
@@ -0,0 +1,65 @@
+const lazyLoad = (imageWrapper) => {
+  const setLoaded = (wrapper) => {
+    wrapper.classList.add("ratioImage--loaded");
+  };
+
+  const image = imageWrapper.querySelector("img");
+  if (image.complete) {
+    setLoaded(imageWrapper);
+    console.log("complete");
+  } else
+    image.onload = () => {
+      setLoaded(imageWrapper);
+    };
+};
+
+document.querySelectorAll(".ratioImage").forEach(lazyLoad);
+
+const slickSlider = document.querySelector(".slickSlider");
+if (slickSlider) {
+  $(slickSlider).slick({
+    dots: true,
+    slidesToShow: 2.5,
+    slidesToScroll: 0.5,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1360,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1.5,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 840,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  $(slickSlider).on("setPosition", () => {
+    slickSlider
+      .querySelectorAll(".ratioImage:not(.ratioImage--loaded)")
+      .forEach(lazyLoad);
+  });
+}
