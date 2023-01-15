ee95582044eb5e67ff745d311bf2492eb491d6f8
Hyzyr <avezberdiyew@gmail.com>
2023-01-15 13:42:12 +0500

    slider make 1 row in mobile

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,30 +2,26 @@ const slickSlider = document.querySelector(".slickSlider");
 if (slickSlider) {
   $(slickSlider).slick({
     dots: true,
-    slidesToShow: 2.5,
-    slidesToScroll: 0.5,
+    slidesToShow: 5,
+    slidesToScroll: 1,
     infinite: true,
     responsive: [
       {
         breakpoint: 1360,
         settings: {
-          slidesToShow: 2,
-          dots: true,
+          slidesToShow: 4,
         },
       },
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 1.5,
-          dots: true,
+          slidesToShow: 3,
         },
       },
       {
         breakpoint: 840,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
-          dots: true,
           arrows: false,
         },
       },
@@ -33,7 +29,6 @@ if (slickSlider) {
         breakpoint: 700,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
           arrows: false,
         },
       },
